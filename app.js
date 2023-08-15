f6f75877c8c8c8b24f76bcf6ed9afe6d46fc1f48
devthiart <devthiart@gmail.com>
2023-08-15 18:30:36 -0300

    add other log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Running game management system... ");
 console.log("connecting to the database...");
 // Other console log for test
-console.log("Paulo make this commit without you see at the class.")
+console.log("Paulo make this commit without you see at the class.");
+console.log("Other log...");
